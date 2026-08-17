901242d0a78b9e70da48063648067ded584db5de
KacperParaniuk <kacparaniuk@gmail.com>
2026-08-16 23:45:01 -0500

    Fixed Commands in Script.

--- Command_Line_Interface/pib-cli/main.py
+++ Command_Line_Interface/pib-cli/main.py
@@ -265,8 +265,8 @@ class PIBShell(cmd.Cmd):
             print("Invalid sensor number. Please enter a number between 1 and 5.")
             return
         else:
-            print("Reading temperature from sensor " + arg + "...")
-            self.pib.send_command(CMD_READ_TC1 + (sensor - 1)) # takes sensor 1 and adds the sensor number - 1 to obtain the correct command.
+            print("Reading cold junction temperature from sensor " + arg + "...")
+            self.pib.send_command(CMD_READ_TC1_CJ1 + (sensor - 1)) # takes sensor 1 and adds the sensor number - 1 to obtain the correct command.
             response = self.pib.read_response()
             if response:
                 print(f"PIB says: {response}" + " Celcius degrees")
@@ -289,6 +289,8 @@ class PIBShell(cmd.Cmd):
             if response:
                 print(f"PIB says: {response}" + " Celcius degrees")
 
+
+
     def do_read_pressure(self, arg):
         """Read the pressure from the payload interface board."""
         try: 
@@ -307,9 +309,9 @@ class PIBShell(cmd.Cmd):
             if response:
                 print(f"PIB says: {response}" + " Pa") 
 
-    def do_read_capacitancez(self, arg):
+    def do_read_capacitance(self, arg):
         """Read the capacitance chip from the payload interface board."""
-        print("Reading pressure from sensor " + arg + "...")
+        print("Reading capacitance from sensor " + arg + "...")
 
         try: 
             sensor = int(arg)
@@ -317,13 +319,25 @@ class PIBShell(cmd.Cmd):
             print("Invalid input. Please enter a number between 1 and 2.")
             return 
         
-        self.pib.send_command(CMD_READ_PT1 + (sensor - 1)) # takes sensor 1 and adds the sensor number - 1 to obtain the correct command.
+        self.pib.send_command(CMD_READ_CAPACITANCE_A1 + (sensor - 1)) # takes sensor 1 and adds the sensor number - 1 to obtain the correct command.
         response = self.pib.read_response()
         if response:
             print(f"PIB says: {response}" + " Pa") 
 
     def do_read_level(self, arg):
          """Read the level of the FAM142 propellant from the payload interface board."""
+        print("Reading propellant level from sensor " + arg + "...")
+
+        try: 
+            sensor = int(arg)
+        except ValueError:
+            print("Invalid input. Please enter a number between 1 and 2.")
+            return 
+        
+        self.pib.send_command(CMD_READ_CATALYST_LEVEL_A1 + (sensor - 1)) # takes sensor 1 and adds the sensor number - 1 to obtain the correct command.
+        response = self.pib.read_response()
+        if response:
+            print(f"PIB says: {response}" + " Pa") 
 
     def do_read_valve_state(self, arg):
         """Read the valve state on the payload interface board."""
@@ -338,9 +352,11 @@ class PIBShell(cmd.Cmd):
             return
         else:
             print("Reading valve " + arg + "...")
-            # imput command.             
+            self.pib.send_command(CMD_READ_VALVE_STATE + (valve - 1)) # takes valve 1 and adds the valve number - 1 to obtain the correct command.
+            response = self.pib.read_response()
+            if response:
+                print(f"Valve State: {response}" + " Pa") 
       
-
         
     def do_run_sequence(self, arg):
         """Run a predefined sequence on the payload interface board."""
@@ -349,6 +365,10 @@ class PIBShell(cmd.Cmd):
 
         if(arg == "fill_accum1"):
             print("Running fill accumulator sequence 1...")
+
+            # we do not have a command for this just yet. 
+            # COMING SOON!!! 
+
             # Here add the code to run sequence 1 on the PIB
 
         if(arg == "run_espray"):
